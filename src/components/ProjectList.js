2cdae8069f6129b64dd002096443c1349b4ce36a
+ <rgigs123@gmail.com>
2025-08-01 21:06:22 +0300

    react

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,20 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+  console.log(projects); // this logs the prop passed down from App
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projects.map((project) => (
+          <ProjectItem
+            key={project.id}
+            name={project.name}
+            technologies={project.technologies}
+          />
+        ))}
+      </div>
     </div>
   );
 }
